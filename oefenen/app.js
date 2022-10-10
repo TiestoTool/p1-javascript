e2c3f036c8a04b43f986780208a20de7f1c03640
Tiesto Tool <9019545@student.zadkine.nl>
2022-10-10 12:35:38 +0200

    a

--- oefenen/app.js
+++
@@ -1,9 +0,0 @@
-
-function kleur_aanpassen() {
-
-    let title = "kutJavascript";
-    let element =document.getElementById(title);
-    element.style.color = "red";
-    document.getElementById(title).style.fontWeight = "normal";
-    
-}
