d2a68700058594ce2f1fe72a732540ea32e5c1b2
Tiesto Tool <9019545@student.zadkine.nl>
2022-11-14 13:37:03 +0100

    1

--- spelletje/app.js
+++ spelletje/app.js
@@ -4,8 +4,17 @@ let counter = 0;
 
 
 function spel(){
- move.style.left = "30rem"
+
  counter += 10;
  titel.innerText = counter;
-}
 
+const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
+const square= document.querySelector('#square');
+setInterval(() => {
+   
+   square.style.left= getRandom(0, 50 - 10)+'rem'; // 👈🏼 Horizontally
+   square.style.top = getRandom(0, 40 - 20)+'rem'; // 👈🏼 Vertically
+    
+}, 1000); // every 1/2 second
+}
